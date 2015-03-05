46411ff2b757099d6786dae33066a1c6ad75ffd0
Ari Cohen <arilovesrugby@gmail.com>
2015-03-04 19:49:36 -0500

    added pages for bio, foster, undoc; some homepage tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
  root "pages#home"
  get "about" => "pages#about" #creates about path
+ get "bio" => "pages#bio"
+ get "foster" => "pages#foster"
+ get "undoc" => "pages#undoc"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
