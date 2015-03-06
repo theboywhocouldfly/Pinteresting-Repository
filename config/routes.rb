e20829281f3c7ba62c8c074469d79dcc8dd7d5aa
Ari Cohen <arilovesrugby@gmail.com>
2015-03-06 18:08:08 -0500

    fixed image alignment in carousel, added break between nav and carousel, increased height of upper navbar to accommodate eventual logo

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
  get "undoc" => "pages#undoc"
  get "education" => "pages#education"
  get "getinvolved" => "pages#getinvolved"
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
